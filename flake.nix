e5f56802beee0c8767255d6a7f210c57a98998f8
Stijn Ruts <git@stijnruts.be>
2026-07-25 09:54:05 +0200

    devShell

--- flake.nix
+++ flake.nix
@@ -2,6 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
-  outputs = { self, nixpkgs }: {
-  };
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.hello ];
+      };
+    };
 }
