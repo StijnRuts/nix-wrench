ebb019a2fa44a4bce2c2f03fedb7bf6539132843
Stijn Ruts <git@stijnruts.be>
2026-08-06 11:45:24 +0200

    Only watch .nix

--- bolts/extra/container-watch.sh
+++ bolts/extra/container-watch.sh
@@ -5,11 +5,9 @@ name="${1:?Usage: $0 <name> <config_name> <project_mount_target>}"
 config="${2:?Usage: $0 <name> <config_name> <project_mount_target>}"
 project_mount_target="${3:?Usage: $0 <name> <config_name> <project_mount_target>}"
 
-project_root="$(git rev-parse --show-toplevel 2>/dev/null || realpath .)"
-
 echo "*** Rebuild on file changes"
 watchexec \
-  --watch "$project_root" \
+  --exts nix \
   --postpone \
   --restart \
   -- incus exec "$name" \
